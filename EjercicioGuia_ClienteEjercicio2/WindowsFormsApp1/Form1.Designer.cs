cfa1bb464be8777cce5ccded3dbc9c30b1191f0c
Marcel Marco <marco.marcel@estudiantat.upc.edu>
2022-04-03 18:32:43 +0200

    EjercicioGuiaL3

---
+++ EjercicioGuia_ClienteEjercicio2/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Temperatura = new System.Windows.Forms.TextBox();
+            this.Cel = new System.Windows.Forms.RadioButton();
+            this.Far = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(567, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Desconectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Temperatura
+            // 
+            this.Temperatura.Location = new System.Drawing.Point(307, 159);
+            this.Temperatura.Name = "Temperatura";
+            this.Temperatura.Size = new System.Drawing.Size(210, 26);
+            this.Temperatura.TabIndex = 2;
+            // 
+            // Cel
+            // 
+            this.Cel.AutoSize = true;
+            this.Cel.Location = new System.Drawing.Point(307, 191);
+            this.Cel.Name = "Cel";
+            this.Cel.Size = new System.Drawing.Size(85, 24);
+            this.Cel.TabIndex = 3;
+            this.Cel.TabStop = true;
+            this.Cel.Text = "Celsius";
+            this.Cel.UseVisualStyleBackColor = true;
+            // 
+            // Far
+            // 
+            this.Far.AutoSize = true;
+            this.Far.Location = new System.Drawing.Point(307, 221);
+            this.Far.Name = "Far";
+            this.Far.Size = new System.Drawing.Size(102, 24);
+            this.Far.TabIndex = 4;
+            this.Far.TabStop = true;
+            this.Far.Text = "Farenheit";
+            this.Far.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(307, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 41);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Enviar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Far);
+            this.Controls.Add(this.Cel);
+            this.Controls.Add(this.Temperatura);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Temperatura;
+        private System.Windows.Forms.RadioButton Cel;
+        private System.Windows.Forms.RadioButton Far;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
